1292d6828d805a59c663cf0fa15483ad104e7b83
Jianchi Chen <jianchi_ch@outlook.com>
2025-12-02 16:35:57 +0800

    feat: configure github page deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,41 +9,43 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
+    plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
 
-    // 1. 自动引入组件
-    Components({
-      resolvers: [NaiveUiResolver()], // 关键
-      dts: true, // 生成 .d.ts，TS 有提示
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+        // 1. 自动引入组件
+        Components({
+            resolvers: [NaiveUiResolver()], // 关键
+            dts: true, // 生成 .d.ts，TS 有提示
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
 
-  // 配置 Vite 代理
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5173", // ← 这里填你的后端端口
-        changeOrigin: true,
-        rewrite: (path) => path,
-      },
+    // 配置 Vite 代理
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:5173", // ← 这里填你的后端端口
+                changeOrigin: true,
+                rewrite: (path) => path,
+            },
+        },
     },
-  },
 
-  // 避免 Vite 在构建时错误地处理 Naive UI 的某些模块。
-  optimizeDeps: {
-    exclude: ["naive-ui"],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
+    // 避免 Vite 在构建时错误地处理 Naive UI 的某些模块。
+    optimizeDeps: {
+        exclude: ["naive-ui"],
     },
-  },
+    build: {
+        commonjsOptions: {
+            include: [/node_modules/],
+        },
+    },
+
+    base: "/myBlog/",
 });
