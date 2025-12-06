7ed212edaf177612253d83544e45c37026617924
Jianchi-Chen <Jianchi-Chen@users.noreply.github.com>
2025-12-06 08:01:38 +0000

    deploy: 894f5d9cc493371cd00795c3588575f59c94f4e9

--- assets/account-BOj0_k8S.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./index-D88i5dwZ.js";const s=t=>e.post("/api/register",t),n=t=>e.post("/api/login",t),o=t=>e.get("/api/users",{params:{limit:t}}),c=t=>e.delete(`/api/users/${t}`),a=t=>e.put("/api/editAccount",t);export{a as E,c as d,o as f,n as l,s as r};
